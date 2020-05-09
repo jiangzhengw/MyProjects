534fa72bbdb48a3af0e9e72c2f267d564fc14e95
jiangzhengw <552165844zjx**>
2020-05-09 16:11:15 +0800

    2020-5-9 commit_01

--- jzwpytest/test_div.py
+++ jzwpytest/test_div.py
@@ -1,26 +1,50 @@
+import pytest
+
 from jzwpytest.div import div
 
 
 # type()查看数据类型的方法
 # 设置修改pycharm快捷键，ctrl+alt+s，搜索keymap，修改
+
+# pytest --junitxml=jzwpytest/result/junit.xml --alluredir=jzwpytest/allure_results jzwpytest
+# allure serve jzwpytest\allure_results
+
 class TestBreakWork:
+    # 注解实现分组执行
+    @pytest.mark.happy
     def test_div_int(self):
         assert div(10, 2) == 5
         assert div(10, 5) == 2
         assert div(1000000000, 1) == 1000000000
 
+    # pytest 参数化
+    @pytest.mark.happy
+    @pytest.mark.parametrize("num1, num2, res", {
+        (10, 2, 5),
+        (10, 5, 2),
+        (10, 5, 3),
+        (10000, 1, 10000)
+    })
+    def test_div_int_param(self, num1, num2, res):
+        assert div(num1, num2) == res
+
+    @pytest.mark.happy
     def test_div_float(self):
         # 结果是浮点数，a，b本身就是浮点数
         assert div(10, 3) == 3.33
         assert div(10.2, 0.2) == 50.1
 
+    @pytest.mark.exception
     def test_div_exc(self):
         # 不会出现商业报错，崩溃
         assert div(10, 'a')
         assert div('a', 10)
 
+    @pytest.mark.zero
     def test_div_zero(self):
-        assert div(10, 0) == None
+        with pytest.raises(ZeroDivisionError):
+            div(10, 0)
 
+    @pytest.mark.fu
     def test_div_fu(self):
         assert div(10, -1) == -10
